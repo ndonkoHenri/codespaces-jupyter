27861d3d729d28edeb6be7933babacf19df1dd79
ndonkoHenri <robotcoder4@protonmail.com>
2024-01-25 18:29:33 +0100

    row

--- code/url-shortener.py
+++ code/url-shortener.py
@@ -1,3 +1,48 @@
 import flet as ft
 import pyshorteners  # pip install pyshorteners
 
+shortener = pyshorteners.Shortener()
+
+
+class ShortLinkRow(ft.Row):
+    # a row containing the shortened url, and two buttons ('copy', and 'open in browser')
+
+    def __init__(self, shortened_link):
+        """
+        We create a new class called `ShortenedLinkRow` that inherits from `ft.Row`.
+        The constructor takes two arguments/parameters: `shortened_link` and `source`.
+
+        :param shortened_link: the shortened link
+        """
+        super().__init__()  # required when overwriting the constructor
+
+        self.alignment = "center"  # center the contents of this row
+
+        # the controls/content of our Row
+        self.controls = [
+            ft.Text(value=shortened_link, size=16, selectable=True, italic=True),
+            ft.IconButton(
+                icon=ft.icons.COPY,  # the icon to be showed
+                on_click=lambda e: self.copy(shortened_link),
+                # when this button is clicked, call the `copy` method, passing the shortened link as parameter
+                bgcolor=ft.colors.BLUE_700,
+                tooltip="copy"  # to be showed when hovering on this button
+            ),
+            ft.IconButton(
+                icon=ft.icons.OPEN_IN_BROWSER_OUTLINED,  # the icon to be showed
+                tooltip="open in browser",  # to be showed when hovering on this button
+                on_click=lambda e: e.page.launch_url(shortened_link)
+                # when this button is clicked, open a browser tab with that shortened link
+            )
+        ]
+
+    def copy(self, value):
+        """
+        It copies the given value to the clipboard, and opens a Snackbar to inform the user.
+        :param value: The value to be copied to the clipboard
+        """
+        self.page.set_clipboard(value)
+        self.page.show_snack_bar(ft.SnackBar(ft.Text("Link copied to clipboard!")))
+
+
+
